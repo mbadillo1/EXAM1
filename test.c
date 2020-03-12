b0fb8d0a53a71ba0deadfc5be394520c406a5925
mbadillo1 <mbadillo1@csc208.csudh.edu>
2020-03-11 17:14:15 -0700

    files

---
+++ test.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE20
+ int sides(int y);
+ int rolla(int x);
+int dice(void);
+int rolld;
+void printer;
+
+
+
+int main (void)
+{
+sides(int y);
+dice(void);
+
+return EXIT_SUCCESS;
+}
+int sides(int y[SIZE] )
+{
+sides[0];	
+int y=0;	
+printf("enter the sides you want on dice no more than 20 \n");
+scanf("%d",&y);
+
+ if(y<=SIZE)
+ {
+  puts(sides[y ]);		
+	
+ } 
+ else
+	{
+		printf("not the correct input \n");
+	}
+	
+}	
+int rolla(void )
+{
+int x=0;
+
+printf("how mnay time would you like to roll the dice \n ");
+scanf("%d",&x);
+}
+int dice(void)
+
+{
+
+
+
+}
+
+void printer ()
+{
+}
+
+int rolld ()
+
+{
+
+}
